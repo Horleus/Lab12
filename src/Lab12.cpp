336489921ca4c48e96f46ef61f926ba65209110c
win <win@WINPC>
2020-05-22 00:18:40 +0300

    Ready

--- src/Lab12.cpp
+++ src/Lab12.cpp
@@ -1,7 +1,23 @@
+#include <math.h>
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int i, n;
+	double res;
+	setlocale(LC_ALL, "Ukrainian");
+	cout << "Задайте кiлькiсть повторiв циклу:";
+	cin >> n;
+	if (n >= 2) {
+		res = 1;
+		i = 2;
+		while (i <= n) {
+			res = res * (1.0 - 1.0 / pow(i, 2));
+			i++;
+		}
+		cout << endl << "Результат = " << res << endl;
+	} else
+		cout << "Кiлькiсть повторiв не задана!" << endl;
 	return 0;
 }
